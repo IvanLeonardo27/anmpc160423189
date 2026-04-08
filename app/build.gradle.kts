9a132cbbe842a6dd4bc0d6343b28829b3e8396b8
Leon27Ivan <ivanleonardo415@gmail.com>
2026-04-08 08:13:12 +0700

    view binding telah diaktifkan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.nmpc.projectgit"
     compileSdk = 36
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.nmpc.projectgit"
         minSdk = 24
